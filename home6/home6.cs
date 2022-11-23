2226b1fdcd5ea8be8bbb95fd98e4b9d141569bdc
Danis <danshadance@mail.ru>
2022-11-23 11:05:12 +0300

    Проверка числа на четность

---
+++ home6/home6.cs
@@ -0,0 +1,20 @@
+﻿int a = 4;
+
+if( a % 2 == 0);
+{
+    Console.WriteLine("Число a - четное");
+}
+
+int b = -3;
+
+if( b % 2 != 0);
+{
+    Console.WriteLine("Число b - нечетное");
+}
+
+int с = 7;
+
+if( с % 2 != 0);
+{
+    Console.WriteLine("Число с - нечетное");
+}
