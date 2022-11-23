f5361e363ede6172fa347c7b1f08bab73cd1ecb0
Danis <danshadance@mail.ru>
2022-11-23 10:22:52 +0300

    home2

---
+++ home2/home2.cs
@@ -0,0 +1,33 @@
+﻿int a = 5;
+int b = 7;
+int c = 2;
+int d = 10;
+int e = -9;
+int f = -3;
+
+if(a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
+
+if(c > d)
+{
+    Console.WriteLine(c);
+}
+else
+{
+    Console.WriteLine(d);
+}
+
+if(e > f)
+{
+    Console.WriteLine(e);
+}
+else
+{
+    Console.WriteLine(f);
+}
